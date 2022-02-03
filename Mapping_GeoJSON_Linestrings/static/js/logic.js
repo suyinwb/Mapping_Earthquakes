c65613ad67549f5a8d71618585fdeb3ebf8c9d40
suyinwb <wong.suyin@gmail.com>
2022-02-03 00:28:45 -0500

    line string with airline & destination

--- Mapping_GeoJSON_Linestrings/static/js/logic.js
+++ Mapping_GeoJSON_Linestrings/static/js/logic.js
@@ -46,9 +46,20 @@ L.control.layers(baseMaps).addTo(map);
 // Accessing the Toronto airline routes GeoJSON URL.
 let torontoData = "https://raw.githubusercontent.com/suyinwb/Mapping_Earthquakes/main/torontoRoutes.json";
 
+// Create a style for the lines.
+let myStyle = {
+    color: "#ffffa1",
+    weight: 2
+}
+
 // Grabbing our GeoJSON data.
 d3.json(torontoData).then(function(data) {
     console.log(data);
-  // Creating a GeoJSON layer with the retrieved data.
-  L.geoJSON(data).addTo(map);
+    // Creating a GeoJSON layer with the retrieved data.
+    L.geoJSON(data, {
+      style: myStyle,
+      onEachFeature: function(feature, layer) {
+        layer.bindPopup("<h2> Airline: " + feature.properties.airline + "</h2> <hr> <h3>Destination: " + feature.properties.dst +  "</h3>");
+      }
+  }).addTo(map);
 });
